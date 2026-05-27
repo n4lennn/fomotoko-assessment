9094279d4fdd19907a279e48a712d5e1808ace35
Nalendra <6n4len123@gmail.com>
2026-05-27 10:29:58 +0700

    fix: tambah route GET products dan hapus duplikat route orders

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 // route produk
-Route::get('/orders', [OrderController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::patch('/products/{product}', [ProductController::class, 'update']);
